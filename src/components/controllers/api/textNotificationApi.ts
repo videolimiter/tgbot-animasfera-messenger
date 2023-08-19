97e5d95324431d79c587859bb49353edb39e7e46
Roman Shtepin <videolimiter@gmail.com>
2023-08-20 01:53:16 +0500

    remade Timeout send

--- src/components/controllers/api/textNotificationApi.ts
+++ src/components/controllers/api/textNotificationApi.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express"
 import TelegramBot from "../../telegram/telegramBot"
 import { SendTextMessage, SendTextMessageType } from "./validations"
-import sleep from "../../helpers/sleep"
+
 
 const textNotificationApi = (
   req: Request,
@@ -9,7 +9,7 @@ const textNotificationApi = (
   next: NextFunction
 ) => {
   const bot = TelegramBot.getInstance(process.env.TELEGRAM_TOKEN || "")
-  
+
   const data = req.body
   SendTextMessage.parse(data)
   const { chatId, text }: SendTextMessageType = req.body
@@ -21,13 +21,20 @@ const textNotificationApi = (
 
     console.log("singleID: ", chatId)
   } else if (Array.isArray(chatId)) {
-    chatId.forEach(async (chatId: number) => {
-      bot.telegram
-        .sendMessage(chatId, text, { parse_mode: "HTML" })
-        .catch((err) => console.log(err))
-      await sleep(35)
-    })
     console.log("chatId is an array:", chatId)
+
+    const sendMessages = async (
+      chatIds: number[],
+      text: string
+    ): Promise<void> => {
+      for (const chatId of chatIds) {
+        await new Promise((resolve) => setTimeout(resolve, 35)) // Задержка
+        bot.telegram
+          .sendMessage(chatId, text, { parse_mode: "HTML" })
+          .catch((err) => console.log(err))
+      }
+    }
+    sendMessages(chatId, text).then(() => console.log("Отправка окончена"))
   } else {
     return console.log("Invalid chatId type")
   }
